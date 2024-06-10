1fe6f6127a31c7acb2fd305e6248fed7b4bd8c0d
Lukasz <lukaszkotowski@protonmail.com>
2024-06-10 04:35:36 -0400

    v5

--- barber.c
+++ barber.c
@@ -4,6 +4,7 @@
 #include <semaphore.h>
 #include <unistd.h>
 #include <time.h>
+#include <stdbool.h>
 
 #define MAX_CHAIRS 10
 
@@ -16,6 +17,24 @@ int total_chairs;
 int rejections = 0;
 int current_customer = -1;
 int waiting_customers[MAX_CHAIRS];
+int rejected_customers[100];
+int total_rejections = 0;
+bool info_mode = false;
+
+void print_info() {
+    if (info_mode) {
+        printf("Kolejka oczekujących: ");
+        for (int i = 0; i < waiting; i++) {
+            printf("%d ", waiting_customers[i]);
+        }
+        printf("\n");
+        printf("Klienci, którzy nie dostali się do gabinetu: ");
+        for (int i = 0; i < total_rejections; i++) {
+            printf("%d ", rejected_customers[i]);
+        }
+        printf("\n");
+    }
+}
 
 void* barber_thread(void* arg) {
     while (1) {
@@ -33,6 +52,7 @@ void* barber_thread(void* arg) {
         sem_post(&barber); // Powiadomienie fryzjera
 
         printf("Strzyżenie klienta: %d Poczekalnia: %d/%d [Fotel: %d]\n", current_customer, waiting, total_chairs, current_customer);
+        print_info();
 
         sleep(3); // Strzyżenie klienta
 
@@ -51,12 +71,16 @@ void* customer_thread(void* arg) {
         waiting_customers[waiting] = id;
         waiting++;
         printf("Klient %d w poczekalni. Poczekalnia: %d/%d\n", id, waiting, total_chairs);
+        print_info();
         sem_post(&customers); // Nowy klient
         pthread_mutex_unlock(&mutex);
         sem_wait(&barber); // Czekaj na fryzjera
     } else {
         rejections++;
+        rejected_customers[total_rejections] = id;
+        total_rejections++;
         printf("Rezygnacja: %d Poczekalnia: %d/%d [Fotel: %d]\n", rejections, waiting, total_chairs, current_customer);
+        print_info();
         pthread_mutex_unlock(&mutex);
     }
     free(arg);
@@ -64,8 +88,8 @@ void* customer_thread(void* arg) {
 }
 
 int main(int argc, char* argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <number_of_chairs>\n", argv[0]);
+    if (argc < 2 || argc > 3) {
+        fprintf(stderr, "Usage: %s <number_of_chairs> [-info]\n", argv[0]);
         return 1;
     }
 
@@ -73,6 +97,10 @@ int main(int argc, char* argv[]) {
     if (total_chairs > MAX_CHAIRS) {
         fprintf(stderr, "Maksymalna liczba krzeseł to %d\n", MAX_CHAIRS);
         return 1;
+    }
+
+    if (argc == 3 && strcmp(argv[2], "-info") == 0) {
+        info_mode = true;
     }
 
     pthread_t barber_tid;
@@ -89,7 +117,7 @@ int main(int argc, char* argv[]) {
     for (int i = 0; i < 100; i++) {
         int* id = malloc(sizeof(int));
         *id = i + 1;
-        usleep(rand() % 1000000); // Klienci przychodzą w losowym czasie
+        usleep(rand() % (3000000)); // Klienci przychodzą w losowym czasie (od 0 do 3 sekund)
         pthread_create(&customer_tid[i], NULL, customer_thread, id);
     }
 
